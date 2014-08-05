da51556fa1e800289979590640a26db20e662388
Gioh Kim <gurugio@hanmail.net>
2014-08-05 23:22:43 +0900

    ch2

---
+++ 2.1.2.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname 2.1.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+(sqrt 4)
+(sqrt 2)
+(sqrt -1)
+(tan 0)
+;
+;2
+;#i1.4142135623730951
+;0+1i
+;0
